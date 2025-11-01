7156e901bb89beafcc6fca87e5a9cd9eb6a44604
phuc-12 <phucqua147@gmail.com>
2025-11-01 14:09:14 +0700

    fix dropdown

--- app/Models/InvoiceDetail.php
+++ app/Models/InvoiceDetail.php
@@ -7,18 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceDetail extends Model
 {
     //
-    protected $primaryKey = 'invoice_detail_id';
-    protected $fillable = ['invoice_id', 'booking_id', 'sub_total', 'quantity'];
     
-    // Chi tiết này thuộc về 1 booking
-    public function booking()
-    {
-        return $this->belongsTo(Bookings::class, 'booking_id', 'booking_id');
-    }
-    
-    // Chi tiết này thuộc về 1 hóa đơn
-     public function invoice()
-    {
-        return $this->belongsTo(Invoice::class, 'invoice_id', 'invoice_id');
-    }
 }
